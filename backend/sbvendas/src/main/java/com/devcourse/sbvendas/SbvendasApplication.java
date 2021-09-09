52ae45d6076bf2c262fceb681a1f47f6ec47c1b9
Luan Reis <luanreis2202@gmail.com>
2021-09-09 16:09:24 -0300

    Project Created

---
+++ backend/sbvendas/src/main/java/com/devcourse/sbvendas/SbvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devcourse.sbvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbvendasApplication.class, args);
+	}
+
+}
